9bc4f1cde17f7a98b1c26a31e615c99415c96503
Renan Joppert <renanljp@gmail.com>
2024-05-14 18:38:09 -0300

    fix: change http method verb to update product

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,10 +16,10 @@ Route::group([
         'as' => 'products.',
         'prefix' => 'products'
     ], function () {
-        Route::get('/', [ProductController::class, 'index'])->name('index');
         Route::post('/', [ProductController::class, 'store'])->name('store');
+        Route::get('/', [ProductController::class, 'index'])->name('index');
         Route::get('/{product}', [ProductController::class, 'show'])->name('show');
-        Route::put('/{product}', [ProductController::class, 'update'])->name('update');
+        Route::post('/{product}', [ProductController::class, 'update'])->name('update');
         Route::delete('/{product}', [ProductController::class, 'destroy'])->name('destroy');
     });
 });
