be6e33e0319122859440826add5cdbe6c6ec7b16
Renan Joppert <renanljp@gmail.com>
2024-05-13 23:34:57 -0300

    feat: base structure to Product Module

---
+++ backend/database/factories/ProductFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use GuzzleHttp\Client;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $unsplashUrl = 'https://source.unsplash.com/random/800x600';
+
+        $client = new Client();
+
+        $imageContents = $client->get($unsplashUrl)->getBody()->getContents();
+        $imageName = Str::random(10) . '.jpg';
+        $imagePath = 'products/' . $imageName;
+
+        Storage::disk('public')->put($imagePath, $imageContents);
+
+        return [
+            'name' => $this->faker->word,
+            'description' => $this->faker->sentence,
+            'price' => $this->faker->randomFloat(2, 1, 100),
+            'due_date' => $this->faker->dateTimeBetween('now', '+1 year'),
+            'image' => $imagePath
+        ];
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::group([
+    // 'middleware' => 'auth'
+], function () {
+    Route::group([
+        'as' => 'products.',
+        'prefix' => 'products'
+    ], function () {
+        Route::get('/', [ProductController::class, 'index'])->name('index');
+        Route::post('/', [ProductController::class, 'store'])->name('store');
+        Route::get('/{product}', [ProductController::class, 'show'])->name('show');
+        Route::put('/{product}', [ProductController::class, 'update'])->name('update');
+        Route::delete('/{product}', [ProductController::class, 'destroy'])->name('destroy');
+    });
+});
+
+
+
