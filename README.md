dda5232eb4536192862b4a09ca402f300b63b37d
Renan Joppert <renanljp@gmail.com>
2024-05-15 09:23:52 -0300

    feat: documentation

---
+++ README.md
@@ -0,0 +1,100 @@
+# Instalação
+Clone o repositório em seu computador
+<pre>
+git cone git@github.com:renanjoppert1/innyx-fullstack-developer-test.git
+</pre>
+
+Depois de clonar, vamos precisar fazer algumas configurações no nosso projeto.
+
+## Variáveis de Ambiente
+
+Vamos configurar as nossas variáveis de ambiente. 
+
+### Arquivo .env na raiz do projeto
+Primeiro passo será copiar o arquivo .env.example para .env na raiz do projeto. 
+
+Neste arquivo, você irá definir os dados de banco de dados. Defina:
+<pre>
+DB_DATABASE
+DB_USERNAME
+DB_PASSWORD
+</pre>
+
+### Arquivo .env na pasta backend
+Copie o arquivo .env.example para .env dentro da pasta backend. Repita as mesmas variáveis de banco de dados definido no passo anterior para este arquivo. 
+
+### Arquivo .env na pasta frontend
+Copie o arquivo .env.example para .env dentro da pasta frontend.
+
+## Subindo a Aplicação
+Para subir a aplicação utilizando docker, basta no terminal na raiz do projeto executar:
+<pre>
+docker-compose up -d
+</pre>
+
+Após o docker lançar a aplicação, você deverá executar alguns passos:
+
+### Instalar dependências do Laravel
+Em seu terminal execute
+<pre>
+docker-compose exec app composer install
+</pre>
+
+### Rodar a seeding no banco de dados
+Para popular o banco de dados do projeto, execute em seu terminal:
+<pre>
+docker-compose exec app php artisan migrate:fresh --seed
+</pre>
+
+### Instalar dependências do Frontend
+Em seu terminal dentro da pasta frontend execute
+<pre>
+npm i
+</pre>
+
+### Verificando portas
+Para verificar as portas que a aplicação está rodando, basta executar no terminal:
+<pre>
+docker-compose ps
+</pre>
+
+Ele irá mostrar algo parecido com:
+<pre>
+teste-innyx_app_1          docker-php-entrypoint php-fpm    Up      9000/tcp                                             
+teste-innyx_mysql_1        docker-entrypoint.sh mysqld      Up      0.0.0.0:32780->3306/tcp,:::32780->3306/tcp, 33060/tcp
+teste-innyx_nginx_1        /docker-entrypoint.sh ngin ...   Up      0.0.0.0:32771->80/tcp,:::32771->80/tcp               
+teste-innyx_phpmyadmin_1   /docker-entrypoint.sh apac ...   Up      0.0.0.0:32781->80/tcp,:::32781->80/tcp  
+</pre>
+
+### Rodando o frontend
+Em seu terminal dentro da pasta frontend execute:
+<pre>
+npm run dev
+</pre>
+
+
+## Documentação da API
+
+Para gerar a documentação da API para uma collection no postman, execute em seu terminal:
+<pre>
+docker-compose exec app php artisan export:postman
+</pre>
+
+A collection será criada e salva na pasta `backed/storage/app/postman` no arquivo collection.json. 
+
+Após realizar a importação faça o login na API para receber o token de autenticação. As credenciais estão fixadas no final da documentação.
+
+Uma vez com acesso ao token, vá nas configurações da collection dentro do postman, em variables e  insira na coluna Current Value o seu token de autenticação. 
+
+Agora você conseguirá fazer qualquer requisição dentro da collection sem precisar inserir o token manualmente em cada requisição. 
+
+## Usuários
+<pre>
+email => admin@innyx.com
+senha => 123456
+</pre>
+
+<pre>
+email => user@innyx.com
+senha => 123456
+</pre>
--- frontend/src/views/UsersView.vue
+++ frontend/src/views/UsersView.vue
@@ -1,78 +1,91 @@
 <script lang="ts" setup>
-import {onMounted, ref, watch} from "vue";
+import { onMounted, ref, watch } from 'vue'
 
-import AdminLayout from "@/components/AdminLayout.vue";
-import PageTitle from "@/components/App/PageTitle.vue";
+import AdminLayout from '@/components/AdminLayout.vue'
+import PageTitle from '@/components/App/PageTitle.vue'
 
 import axios from 'axios'
 
-import {useToast} from "vue-toastification";
-
+import { useToast } from 'vue-toastification'
 const toast = useToast()
 
+interface User {
+  id?: String
+  name: String
+  email: String
+  password: String
+}
 
 const loading = ref(false)
 const tableData = ref([])
 const currentPage = ref(1)
-const handleFetchData = async (page = currentPage.value
-) => {
-
+const handleFetchData = (page = currentPage.value) => {
   loading.value = true
-  await axios.get(`${import.meta.env.VITE_API_URL}/users?page=${page}`, {headers: {authorization: `Bearer ${localStorage.getItem('token')}`}}).then(res => {
-    tableData.value = res.data
-  }).catch(err => {
-    if (err.response?.data?.message !== undefined) {
-      toast.error(err.response.data.message);
-    } else {
-      toast.error("Houve um erro ao se comunicar com a API");
-    }
-  }).finally(() => loading.value = false)
+  axios
+    .get(`${import.meta.env.VITE_API_URL}/users?page=${page}`, {
+      headers: { authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+    .then((res) => {
+      tableData.value = res.data
+    })
+    .catch((err) => {
+      if (err.response?.data?.message !== undefined) {
+        toast.error(err.response.data.message)
+      } else {
+        toast.error('Houve um erro ao se comunicar com a API')
+      }
+    })
+    .finally(() => (loading.value = false))
 }
 onMounted(() => handleFetchData())
 
-watch(() => currentPage.value, async (to) => {
-  await handleFetchData()
-})
-
+watch(
+  () => currentPage.value,
+  async (to) => {
+    await handleFetchData()
+  }
+)
 
 const search = ref('')
-const handleSearchSubmit = () => {
-}
+const handleSearchSubmit = () => {}
 
-
-const dataToDelete = ref<Object | null>(null)
+const dataToDelete = ref<User | null>(null)
 const modalDelete = ref<boolean>(false)
 const loadingDelete = ref<boolean>(false)
-const handleModalDelete = (row: object) => {
+const handleModalDelete = (row: User) => {
   dataToDelete.value = row
   modalDelete.value = true
 }
 const handleDelete = async () => {
   loadingDelete.value = true
-  await axios.delete(`${import.meta.env.VITE_API_URL}/users/${dataToDelete.value.id}`, {headers: {authorization: `Bearer ${localStorage.getItem('token')}`}}).then(async res => {
-    toast.success('Usuário excluído com sucesso!')
-    modalDelete.value = false
-    dataToDelete.value = null
-    await handleFetchData(1)
-  }).catch(err => {
-    if (err.response?.data?.message !== undefined) {
-      toast.error(err.response.data.message);
-    } else {
-      toast.error("Houve um erro ao se comunicar com a API");
-    }
-  }).finally(() => {
-    loadingDelete.value = false
-
-  })
+  await axios
+    .delete(`${import.meta.env.VITE_API_URL}/users/${dataToDelete.value.id}`, {
+      headers: { authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+    .then(async (res) => {
+      toast.success('Usuário excluído com sucesso!')
+      modalDelete.value = false
+      dataToDelete.value = null
+      await handleFetchData(1)
+    })
+    .catch((err) => {
+      if (err.response?.data?.message !== undefined) {
+        toast.error(err.response.data.message)
+      } else {
+        toast.error('Houve um erro ao se comunicar com a API')
+      }
+    })
+    .finally(() => {
+      loadingDelete.value = false
+    })
 }
 
-
 const dataToEdit = ref<number | null>(null)
-const formEdit = ref<object>({name: ''})
+const formEdit = ref<User>({ name: '', email: '', password: '' })
 const modalEdit = ref<boolean>(false)
 const loadingEdit = ref<boolean>(false)
 
-const handleModalEdit = (row: object) => {
+const handleModalEdit = (row: User) => {
   dataToEdit.value = row.id
   formEdit.value.name = row.name
   formEdit.value.email = row.email
@@ -81,29 +94,33 @@ const handleModalEdit = (row: object) => {
 
 const handleEdit = async () => {
   loadingEdit.value = true
-  await axios.put(`${import.meta.env.VITE_API_URL}/users/${dataToEdit.value}`, formEdit.value, {headers: {authorization: `Bearer ${localStorage.getItem('token')}`}}).then(async res => {
-    toast.success('Usuário alterado com sucesso!')
-    dataToEdit.value = null
+  await axios
+    .put(`${import.meta.env.VITE_API_URL}/users/${dataToEdit.value}`, formEdit.value, {
+      headers: { authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+    .then(async (res) => {
+      toast.success('Usuário alterado com sucesso!')
+      dataToEdit.value = null
 
-    formEdit.value.name = ''
-    formEdit.value.email = ''
+      formEdit.value.name = ''
+      formEdit.value.email = ''
 
-    modalEdit.value = false
-    await handleFetchData(1)
-  }).catch(err => {
-    if (err.response?.data?.message !== undefined) {
-      toast.error(err.response.data.message);
-    } else {
-      toast.error("Houve um erro ao se comunicar com a API");
-    }
-  }).finally(() => {
-    loadingEdit.value = false
-
-  })
+      modalEdit.value = false
+      await handleFetchData(1)
+    })
+    .catch((err) => {
+      if (err.response?.data?.message !== undefined) {
+        toast.error(err.response.data.message)
+      } else {
+        toast.error('Houve um erro ao se comunicar com a API')
+      }
+    })
+    .finally(() => {
+      loadingEdit.value = false
+    })
 }
 
-
-const formCreate = ref<object>({name: '', email: '', password: ''})
+const formCreate = ref<object>({ name: '', email: '', password: '' })
 const modalCreate = ref<boolean>(false)
 const loadingCreate = ref<boolean>(false)
 
@@ -113,32 +130,36 @@ const handleModalCreate = () => {
 
 const handleCreate = async () => {
   loadingCreate.value = true
-  await axios.post(`${import.meta.env.VITE_API_URL}/users`, formCreate.value, {headers: {authorization: `Bearer ${localStorage.getItem('token')}`}}).then(async res => {
-    toast.success('Usuário criado com sucesso!')
-    formCreate.value = {name: '', email: '', password: ''}
-    modalCreate.value = false
-    await handleFetchData(1)
-  }).catch(err => {
-    if (err.response?.data?.message !== undefined) {
-      toast.error(err.response.data.message);
-    } else {
-      toast.error("Houve um erro ao se comunicar com a API");
-    }
-  }).finally(() => {
-    loadingCreate.value = false
-
-  })
+  await axios
+    .post(`${import.meta.env.VITE_API_URL}/users`, formCreate.value, {
+      headers: { authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
+    .then(async (res) => {
+      toast.success('Usuário criado com sucesso!')
+      formCreate.value = { name: '', email: '', password: '' }
+      modalCreate.value = false
+      await handleFetchData(1)
+    })
+    .catch((err) => {
+      if (err.response?.data?.message !== undefined) {
+        toast.error(err.response.data.message)
+      } else {
+        toast.error('Houve um erro ao se comunicar com a API')
+      }
+    })
+    .finally(() => {
+      loadingCreate.value = false
+    })
 }
-
 </script>
 
 <template>
   <AdminLayout>
     <v-card>
       <div class="tw-p-5">
-        <PageTitle title="Usuários"/>
+        <PageTitle title="Usuários" />
       </div>
-      <hr/>
+      <hr />
       <div class="tw-p-5 tw-grid tw-grid-cols-3 tw-gap-10">
         <v-form @submit.prevent="handleSearchSubmit">
           <!--          <v-text-field v-model="search" clearable label="Pesquise">-->
@@ -148,48 +169,46 @@ const handleCreate = async () => {
           <!--              </v-btn>-->
           <!--            </template>-->
           <!--          </v-text-field>-->
-
         </v-form>
         <div class="tw-col-span-2 tw-flex tw-justify-end">
           <v-btn color="primary" @click="handleModalCreate">Adicionar</v-btn>
         </div>
       </div>
-      <hr/>
+      <hr />
       <div class="tw-p-5">
         <v-table v-if="loading === false">
           <thead>
-          <tr>
-            <th class="text-left">
-              Nome
-            </th>
-            <th class="text-right">
-              Ações
-            </th>
-          </tr>
+            <tr>
+              <th class="text-left">Nome</th>
+              <th class="text-right">Ações</th>
+            </tr>
           </thead>
           <tbody>
-          <tr
-              v-for="row in tableData.data"
-              :key="row.name"
-          >
-            <td>{{ row.name }}</td>
-            <td class="text-right">
-              <v-btn variant="text">
-                <AppIcon icon="mdi-edit-box" height="24px" color="grey" @click="handleModalEdit(row)"/>
-              </v-btn>
+            <tr v-for="row in tableData.data" :key="row.name">
+              <td>{{ row.name }}</td>
+              <td class="text-right">
+                <v-btn variant="text">
+                  <AppIcon
+                    icon="mdi-edit-box"
+                    height="24px"
+                    color="grey"
+                    @click="handleModalEdit(row)"
+                  />
+                </v-btn>
 
-              <v-btn variant="text" color="red" @click="handleModalDelete(row)">
-                <AppIcon icon="mdi:trash" height="24px" color="red"/>
-              </v-btn>
-            </td>
-          </tr>
+                <v-btn variant="text" color="red" @click="handleModalDelete(row)">
+                  <AppIcon icon="mdi:trash" height="24px" color="red" />
+                </v-btn>
+              </td>
+            </tr>
           </tbody>
         </v-table>
         <div class="tw-flex tw-flex-col tw-items-center">
-          <v-pagination v-model="currentPage" :length="tableData.last_page">
-          </v-pagination>
-          <p class="tw-text-sm">Mostrando do {{ tableData.from }} ao {{ tableData.to }} de .Total de Registros
-            {{ tableData.total }}</p>
+          <v-pagination v-model="currentPage" :length="tableData.last_page"> </v-pagination>
+          <p class="tw-text-sm">
+            Mostrando do {{ tableData.from }} ao {{ tableData.to }} de .Total de Registros
+            {{ tableData.total }}
+          </p>
           <pre v-if="false">
             {{ tableData }}
           </pre>
@@ -197,146 +216,116 @@ const handleCreate = async () => {
       </div>
     </v-card>
 
-
-    <v-dialog
-        v-model="modalDelete"
-        width="auto"
-    >
-      <v-card
-          max-width="400"
-          title="Atenção"
-      >
+    <v-dialog v-model="modalDelete" width="auto">
+      <v-card max-width="400" title="Atenção">
         <p class="tw-px-6 tw-py-6">
-          Você deseje mesmo apagar o usuário <strong v-if="dataToDelete !== null">{{ dataToDelete.name }}</strong>?
-          Esta ação poderá não ter mais volta.
+          Você deseje mesmo apagar o usuário
+          <strong v-if="dataToDelete !== null">{{ dataToDelete.name }}</strong
+          >? Esta ação poderá não ter mais volta.
         </p>
         <template v-slot:actions>
+          <v-btn text="Cancelar" @click="modalDelete = false" :disabled="loadingDelete"></v-btn>
           <v-btn
-              text="Cancelar"
-              @click="modalDelete = false"
-              :disabled="loadingDelete"
-          ></v-btn>
-          <v-btn
-              variant="flat"
-              color="red"
-              text="Excluir"
-              @click="handleDelete"
-              :disabled="loadingDelete"
-              :loading="loadingDelete"
+            variant="flat"
+            color="red"
+            text="Excluir"
+            @click="handleDelete"
+            :disabled="loadingDelete"
+            :loading="loadingDelete"
           ></v-btn>
         </template>
       </v-card>
     </v-dialog>
 
-
-    <v-dialog
-        v-model="modalEdit"
-        width="400"
-    >
-      <v-card
-          max-width="400"
-          title="Editar Usuário"
-      >
-
+    <v-dialog v-model="modalEdit" width="400">
+      <v-card max-width="400" title="Editar Usuário">
         <v-form @submit.prevent="handleEdit" class="tw-px-4 tw-py-3 tw-flex tw-flex-col tw-gap-4">
           <v-text-field
-              v-model="formEdit.name"
-              hide-details="auto"
-              label="Nome"
-              :rules="[() => !!formEdit.name || 'O nome é obrigatório']"
-              :readonly="loadingEdit" :loading="loadingEdit"
+            v-model="formEdit.name"
+            hide-details="auto"
+            label="Nome"
+            :rules="[() => !!formEdit.name || 'O nome é obrigatório']"
+            :readonly="loadingEdit"
+            :loading="loadingEdit"
           ></v-text-field>
 
           <v-text-field
-              v-model="formEdit.email"
-              hide-details="auto"
-              label="E-mail"
-              :rules="[() => !!formEdit.email || 'O e-mail é obrigatório']"
-              :readonly="loadingEdit" :loading="loadingEdit"
+            v-model="formEdit.email"
+            hide-details="auto"
+            label="E-mail"
+            :rules="[() => !!formEdit.email || 'O e-mail é obrigatório']"
+            :readonly="loadingEdit"
+            :loading="loadingEdit"
           ></v-text-field>
 
           <v-text-field
-              v-model="formEdit.password"
-              hide-details="auto"
-              label="Senha"
-              type="password"
-              :readonly="loadingEdit" :loading="loadingEdit"
+            v-model="formEdit.password"
+            hide-details="auto"
+            label="Senha"
+            type="password"
+            :readonly="loadingEdit"
+            :loading="loadingEdit"
           ></v-text-field>
         </v-form>
 
-
         <template v-slot:actions>
+          <v-btn text="Cancelar" @click="modalEdit = false" :disabled="loadingDelete"></v-btn>
           <v-btn
-              text="Cancelar"
-              @click="modalEdit = false"
-              :disabled="loadingDelete"
-          ></v-btn>
-          <v-btn
-              variant="flat"
-              color="primary"
-              text="Salvar"
-              @click="handleEdit"
-              :disabled="loadingEdit"
-              :loading="loadingEdit"
+            variant="flat"
+            color="primary"
+            text="Salvar"
+            @click="handleEdit"
+            :disabled="loadingEdit"
+            :loading="loadingEdit"
           ></v-btn>
         </template>
       </v-card>
     </v-dialog>
 
-
-    <v-dialog
-        v-model="modalCreate"
-        width="400"
-    >
-      <v-card
-          max-width="400"
-          title="Criar Usuário"
-      >
+    <v-dialog v-model="modalCreate" width="400">
+      <v-card max-width="400" title="Criar Usuário">
         <v-form @submit.prevent="handleCreate" class="tw-px-4 tw-py-3 tw-flex tw-flex-col tw-gap-4">
           <v-text-field
-              v-model="formCreate.name"
-              hide-details="auto"
-              label="Nome"
-              :rules="[() => !!formCreate.name || 'O nome é obrigatório']"
-              :readonly="loadingCreate" :loading="loadingCreate"
+            v-model="formCreate.name"
+            hide-details="auto"
+            label="Nome"
+            :rules="[() => !!formCreate.name || 'O nome é obrigatório']"
+            :readonly="loadingCreate"
+            :loading="loadingCreate"
           ></v-text-field>
 
           <v-text-field
-              v-model="formCreate.email"
-              hide-details="auto"
-              label="E-mail"
-              :rules="[() => !!formCreate.email || 'O e-mail é obrigatório']"
-              :readonly="loadingCreate" :loading="loadingCreate"
+            v-model="formCreate.email"
+            hide-details="auto"
+            label="E-mail"
+            :rules="[() => !!formCreate.email || 'O e-mail é obrigatório']"
+            :readonly="loadingCreate"
+            :loading="loadingCreate"
           ></v-text-field>
 
           <v-text-field
-              v-model="formCreate.password"
-              hide-details="auto"
-              label="Senha"
-              type="password"
-              :rules="[() => !!formCreate.password || 'O senha é obrigatório']"
-              :readonly="loadingCreate" :loading="loadingCreate"
+            v-model="formCreate.password"
+            hide-details="auto"
+            label="Senha"
+            type="password"
+            :rules="[() => !!formCreate.password || 'O senha é obrigatório']"
+            :readonly="loadingCreate"
+            :loading="loadingCreate"
           ></v-text-field>
         </v-form>
 
-
         <template v-slot:actions>
+          <v-btn text="Cancelar" @click="modalEdit = false" :disabled="loadingDelete"></v-btn>
           <v-btn
-              text="Cancelar"
-              @click="modalEdit = false"
-              :disabled="loadingDelete"
-          ></v-btn>
-          <v-btn
-              variant="flat"
-              color="primary"
-              text="Salvar"
-              @click="handleCreate"
-              :disabled="loadingCreate"
-              :loading="loadingCreate"
+            variant="flat"
+            color="primary"
+            text="Salvar"
+            @click="handleCreate"
+            :disabled="loadingCreate"
+            :loading="loadingCreate"
           ></v-btn>
         </template>
       </v-card>
     </v-dialog>
-
   </AdminLayout>
 </template>
